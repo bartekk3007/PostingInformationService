5ba6c776549782bfa1cf6a50dd0ff7b15d559b4f
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-04-29 23:31:01 +0200

    Add files via upload

---
+++ Klient1/Klient1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Discovery;
+
+namespace Klient1
+{
+    [ServiceContract]
+    public interface IZadanie1
+    {
+        [OperationContract]
+        string ScalNapisy(string a, string b);
+    }
+
+    public class Zadanie1 : IZadanie1
+    {
+        public string ScalNapisy(string a, string b)
+        {
+            return a + b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zad1
+            var host = new ServiceHost(typeof(Zadanie1));
+
+            host.Description.Behaviors.Add(new ServiceDiscoveryBehavior());
+            host.AddServiceEndpoint(
+                new UdpDiscoveryEndpoint("soap.udp://localhost:30703"));
+
+            host.AddServiceEndpoint(
+                typeof(IZadanie1),
+                new NetNamedPipeBinding(),
+                "net.pipe://localhost/zadanie1");
+
+            host.Open();
+            Console.ReadKey();
+            host.Close();
+        }
+    }
+}
